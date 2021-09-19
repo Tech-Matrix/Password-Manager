9b3417774916c614fb861206c48c37899696e869
Aditya Lawankar <lawankaraditya@gmail.com>
2021-09-19 14:40:40 +0530

    added change password reminder feature

--- RUNNINGCODE.py
+++ RUNNINGCODE.py
@@ -1,11 +1,12 @@
 import base64
-
+from datetime import datetime
 #GUI CREATION
 from tkinter import *
+from tkinter import messagebox
 
 window=Tk()
 window.title('Password Manager') #giving the name to the window
-window.geometry("550x300+10+20") #giving the dimensions of the window
+window.geometry("550x400+10+20") #giving the dimensions of the window
 window.config(padx=50, pady=50, bg="midnight blue") #giving the color of the window
 
 
@@ -18,7 +19,6 @@ label = Label(window, bg = "midnight blue")
 label.grid(row=1,column=1)
 label = Label(window, bg = "midnight blue")
 label.grid(row=1,column=1)
-
 
 # declaring string variable
 # for storing name and password
@@ -68,6 +68,72 @@ def decrypt():
       print(message)
       file.close()
 
+def reminder():
+
+  date = str(datetime.today())                   #take current date and time
+  with open("date.txt",'a+') as file:
+    file.write(date[:10])              #write only date in file
+    file.write("\n")
+    file.seek(0)
+    startdate = file.readline()
+    dayspassed = int(startdate[5:7]) + 3  #add 3 months 
+    file.close()
+  
+  today = date[:10]
+
+  if(dayspassed not in [10,11,12]):
+    remind_date = startdate[:5]+'0'+str(dayspassed)+startdate[7:-1]
+  else:
+    remind_date = startdate[:5]+str(dayspassed)+startdate[7:-1]
+
+  if(today == remind_date):  #if reminder occurs in current year only
+    messagebox.showinfo("Reminder",  "It's time to change passwords!")
+
+    #clear all dates in file and keep current date
+    with open("date.txt", 'r+') as fp:
+    
+        lines = fp.readlines()
+    
+        fp.seek(0)
+   
+        fp.truncate()
+
+        fp.writelines(lines[-1:])
+        fp.close()
+    
+  elif(today[:4] != remind_date[:4]):    #if reminder occurs next year
+
+    yearchange = int(remind_date[:4]) + 1
+
+    if(remind_date[5:7] == '13'):    #remind in january
+        remind_date = str(yearchange) + '-01' + remind_date[7:]
+
+        if(today == remind_date):
+            messagebox.showinfo("Reminder:",  "It's time to change passwords!")
+            
+    elif(remind_date[5:7] == '14'):  #remind in february
+        remind_date = str(yearchange) + '-02' + remind_date[7:]
+
+        if(today == remind_date):
+            messagebox.showinfo("Reminder:",  "It's time to change passwords!")
+            
+    elif(remind_date[5:7] == "15"):  #remind in march
+        remind_date = str(yearchange) + "-03" + remind_date[7:]
+        
+        if(today == remind_date):
+            messagebox.showinfo("Reminder:",  "It's time to change passwords!")
+        
+    with open("date.txt", 'r+') as fp:
+    
+        lines = fp.readlines()
+    
+        fp.seek(0)
+   
+        fp.truncate()
+
+        fp.writelines(lines[-1:])
+        fp.close() 
+
 
 L1=Label(window, text="User Name", bg = "light cyan", bd = 4, fg = "midnight blue", font=('calibre',10, 'bold')).grid(row=6)
 
@@ -99,10 +165,12 @@ label.grid(column=7)
 # Button that will call the decrypt function
 decrypt_btn=Button(window,text = 'Click Here To Decrypt', command = decrypt, bg = "light cyan", bd = 4, fg = "midnight blue",font=('calibre',10,'bold'))     
 decrypt_btn.grid(row=14,column=1)
+label = Label(window,bg = "midnight blue")
+label.grid(column=15)
+
+#reminder to change passwords
+L3 = Label(window,text = "You will receive a reminder to change passwords\nevery 3 months.You can choose to ignore this\nwhen you receive the reminder.", bg = "light cyan", bd = 4, fg = "midnight blue", font=('calibre',10, 'bold')).grid(row=18,column=1)
+reminder()
 
 window.mainloop()
-
-
-	
-
 
